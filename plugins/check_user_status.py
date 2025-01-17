5059f43a3cdb5a77e310969e1c7577b69b7c5cb2
rohit8708913 <koyeb67user@gmail.com>
2025-01-17 08:12:26 +0530

    Update check_user_status.py

--- plugins/check_user_status.py
+++ plugins/check_user_status.py
@@ -11,6 +11,6 @@ async def handle_user_status(bot, cmd):
         ).days > ban_status["ban_duration"]:
             await db.remove_ban(chat_id)
         else:
-            await cmd.reply_text("You R Banned!.. Contact @Snowball_Official 😝", quote=True)
+            await cmd.reply_text("You R Banned!.. Contact @rohit_1888 😝", quote=True)
             return
     await cmd.continue_propagation()
