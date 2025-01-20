f5fb8c6218abb457d92d3847f7de8737cd175ee4
rohit8708913 <koyeb67user@gmail.com>
2025-01-21 00:42:10 +0530

    Update utils.py

--- helper/utils.py
+++ helper/utils.py
@@ -215,7 +215,7 @@ async def CompressVideo(bot, query, ffmpegcode, c_thumb):
         )
 
         await ms.edit("🗜 **Compressing...**")
-        cmd = f"""ffmpeg -i "{dl}" {ffmpegcode} -vf "scale=ceil(iw/2)*2:ceil(ih/2)*2" -preset veryfast -threads 4 -max_alloc 2G "{Output_Path}" -y"""
+        cmd = f"""ffmpeg -i "{dl}" {ffmpegcode} -vf "scale=ceil(iw/2)*2:ceil(ih/2)*2" -preset ultrafast -threads 4 -bufsize 64M -movflags +faststart "{Output_Path}" -y"""
         print(f"Running FFmpeg command: {cmd}")  # Debugging
 
         # Run FFmpeg with real-time output logging
