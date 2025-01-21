365cc066f0c4ccce62ad67fc5618bae4efb017ef
rohit8708913 <koyeb67user@gmail.com>
2025-01-21 12:22:21 +0530

    Update utils.py

--- helper/utils.py
+++ helper/utils.py
@@ -215,7 +215,7 @@ async def CompressVideo(bot, query, ffmpegcode, c_thumb):
         )
 
         await ms.edit("🗜 **Compressing...**")
-        cmd = f"""ffmpeg -i "{dl}" -vf "scale=if(gte(iw\,2)*2\,iw\,ceil(iw/2)*2):if(gte(ih\,2)*2\,ih\,ceil(ih/2)*2)" -preset ultrafast -crf 23 -c:v libx264 {ffmpegcode} "{Output_Path}" -y"""
+        cmd = f"""ffmpeg -i "{dl}" {ffmpegcode} -vf "scale=ceil(iw/2)*2:ceil(ih/2)*2,drawtext=text='by @Javpostr':fontcolor=white@0.8:fontsize=48:fontfile=/usr/share/fonts/truetype/dejavu/DejaVuSans-Bold.ttf:x=10:y=h-th-10" -preset ultrafast -threads 4 -bufsize 64M -movflags +faststart "{Output_Path}" -y"""
         print(f"Running FFmpeg command: {cmd}")  # Debugging
 
         # Run FFmpeg with real-time output logging
