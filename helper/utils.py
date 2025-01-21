1213b5f40e601353049d5583903668a8f9d2f676
rohit8708913 <koyeb67user@gmail.com>
2025-01-21 10:12:22 +0530

    Update utils.py

--- helper/utils.py
+++ helper/utils.py
@@ -215,7 +215,7 @@ async def CompressVideo(bot, query, ffmpegcode, c_thumb):
         )
 
         await ms.edit("🗜 **Compressing...**")
-        cmd = f"""ffmpeg -i "{dl}" -preset ultrafast -crf 23 -c:v libx264 {ffmpegcode} "{Output_Path}" -y"""
+        cmd = f"""ffmpeg -i "{dl}" -vf "scale=ceil(iw/2)*2:ceil(ih/2)*2" -preset ultrafast -crf 23 -c:v libx264 {ffmpegcode} "{Output_Path}" -y"""
         print(f"Running FFmpeg command: {cmd}")  # Debugging
 
         # Run FFmpeg with real-time output logging
