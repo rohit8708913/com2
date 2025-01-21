b45d3eb6601282d1c9dd1c0b40573e5a564c7bb6
rohit8708913 <koyeb67user@gmail.com>
2025-01-21 10:20:07 +0530

    Update utils.py

--- helper/utils.py
+++ helper/utils.py
@@ -311,8 +311,12 @@ async def CompVideo(bot, query, ffmpegcode, c_thumb, subtitle_file_path):
 
         await ms.edit("🗜 **Compressing...**")
         cmd = f"""
-ffmpeg -i "{dl}" -vf "scale=ceil(iw/2)*2:ceil(ih/2)*2,subtitles='{subtitle_file_path}',drawtext=text='by @Javpostr':fontcolor=white@0.8:fontsize=48:fontfile=/usr/share/fonts/truetype/dejavu/DejaVuSans-Bold.ttf:x=10:y=h-th-10" \
--preset ultrafast -threads 4 -bufsize 64M -c:v libx264 -crf 30 -pix_fmt yuv420p -c:a libopus -b:a 32k -ac 2 -movflags +faststart "{Output_Path}" -y
+ffmpeg -i "{dl}" -i "{subtitle_file_path}" \
+-vf "scale='if(gt(iw,ih),1920,-1)':'if(gt(iw,ih),-1,1080)',subtitles={subtitle_file_path},drawtext=text='by @Javpostr':fontcolor=white@0.8:fontsize=48:fontfile=/usr/share/fonts/truetype/dejavu/DejaVuSans-Bold.ttf:x=10:y=h-th-10" \
+-c:v libx264 -crf 30 -preset ultrafast -pix_fmt yuv420p \
+-c:a libopus -b:a 32k -ac 2 \
+-c:s mov_text -metadata:s:s:0 language=eng -disposition:s:0 default \
+-movflags +faststart "{Output_Path}" -y
 """
         print(f"Running FFmpeg command: {cmd}")  # Debugging
 
