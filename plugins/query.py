0f9591aecbb06d2df299c8cdb702b7eaa9932c98
rohit8708913 <koyeb67user@gmail.com>
2025-01-23 03:22:24 +0530

    Update query.py

--- plugins/query.py
+++ plugins/query.py
@@ -121,7 +121,7 @@ async def Cb_Handle(bot: Client, query: CallbackQuery):
     "-preset veryfast -c:v libx264 "
     "-x265-params \"bframes=8:psy-rd=1:ref=3:aq-mode=3:aq-strength=0.8:deblock=1,1\" "
     "-pix_fmt yuv420p -crf 30 -c:a libopus -b:a 32k "
-    "-vf \"scale='if(gt(iw/ih,1920/1080),1920,-1)':'if(gt(iw/ih,1920/1080),-1,1080)',"
+    "-vf \"scale='if(gt(iw/ih,1920/1080),floor(iw/2)*2,-1)':'if(gt(iw/ih,1920/1080),-1,floor(ih/2)*2)\","
     "drawtext=text='by @Javpostr':fontcolor=white@0.8:fontsize=48:fontfile=/usr/share/fonts/truetype/dejavu/DejaVuSans-Bold.ttf:x=10:y=h-th-10\" "
     "-map 0:v -map 0:a -ac 2 -ab 32k -vbr 2 -level 3.1 -threads 5"
 )
