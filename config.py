dd59c22d61101ceec3389265afb54586d343c632
rohit8708913 <koyeb67user@gmail.com>
2025-03-13 16:16:42 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ class Config(object):
     # pyro client config
     API_ID    = os.environ.get("API_ID", "22469064")  # ⚠️ Required
     API_HASH  = os.environ.get("API_HASH", "c05481978a217fdb11fa6774b15cba32") # ⚠️ Required
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "7401282886:AAGmyggDNhoAHjfQR7xjKQy2CHiZm8RZrxo") # ⚠️ Required
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "7401282886:AAF9p1Dmxs807IJZzSq84m5LObco0pZR63c") # ⚠️ Required
     FORCE_SUB = os.environ.get('FORCE_SUB', '-1002266025682') # ⚠️ Required
     AUTH_CHANNEL = int(FORCE_SUB) if FORCE_SUB and id_pattern.search(
     FORCE_SUB) else None
@@ -24,7 +24,7 @@ class Config(object):
 
     # wes response configuration     
     WEBHOOK = bool(os.environ.get("WEBHOOK", True))
-    PORT = int(os.environ.get("PORT", "6347"))
+    PORT = int(os.environ.get("PORT", "6349"))
 
 
     caption = """
