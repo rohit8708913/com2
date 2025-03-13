f8ed7951c01fb3242e367c9e0ad2a71aadf8aff2
rohit8708913 <koyeb67user@gmail.com>
2025-03-13 16:13:51 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ class Config(object):
     # pyro client config
     API_ID    = os.environ.get("API_ID", "22469064")  # ⚠️ Required
     API_HASH  = os.environ.get("API_HASH", "c05481978a217fdb11fa6774b15cba32") # ⚠️ Required
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "7942169109:AAFpemq2qXv92PP6XUD1l1YlI4lqLQdw8Bc") # ⚠️ Required
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "7401282886:AAGmyggDNhoAHjfQR7xjKQy2CHiZm8RZrxo") # ⚠️ Required
     FORCE_SUB = os.environ.get('FORCE_SUB', '-1002266025682') # ⚠️ Required
     AUTH_CHANNEL = int(FORCE_SUB) if FORCE_SUB and id_pattern.search(
     FORCE_SUB) else None
@@ -28,11 +28,11 @@ class Config(object):
 
 
     caption = """
-**File Name**: {0}
+File Name: {0}
 
-**Original File Size:** {1}
-**Encoded File Size:** {2}
-**Compression Percentage:** {3}
+Original File Size: {1}
+Encoded File Size: {2}
+Compression Percentage: {3}
 
 __Downloaded in {4}__
 __Encoded in {5}__
